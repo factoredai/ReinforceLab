46549b3025ce157ccd59c19a3972df86ec4d2629
Carlo di Francescantonio Torrado <carlo.dt@hotmail.com>
2023-03-03 17:32:47 -0500

    Update Python version in GHA

--- update_leaderboards.py
+++ update_leaderboards.py
@@ -1,8 +1,15 @@
 import os
 import sys
+from datetime import datetime
+
 print(sys.argv[1:])
 
-for agent_path in sys.argv[1:-1]:
+if len(sys.argv[1:-1]) > 0:
+    for agent_path in sys.argv[1:-1]:
 
-    environment_path = "/".join(agent_path.split("/")[:-1])
-    os.system(f'python {environment_path}/update_leaderboard.py {sys.argv[-1]}')
+        environment_path = "/".join(agent_path.split("/")[:-1])
+        os.system(f'python {environment_path}/update_leaderboard.py {sys.argv[-1]}')
+else:
+    f = open(f"last_update.txt", "w")
+    f.write(f"Last Update (UTC): {datetime.utcnow()}\n\n")
+    f.close()
