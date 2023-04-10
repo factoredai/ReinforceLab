fae0ca2c7baff903b6f54ba8b3bf0e1c7a3cd783
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-04-09 23:52:02 -0500

    Rename QEstimator to MaxQEstimator

--- reinforcelab/update_estimators/__init__.py
+++ reinforcelab/update_estimators/__init__.py
@@ -1,6 +1,6 @@
 from .update_estimator import UpdateEstimator
-from .q_estimator import QEstimator
+from .max_q_estimator import MaxQEstimator
 from .double_q_estimator import DoubleQEstimator
 from .sarsa_estimator import SARSAEstimator
 
-__all__ = [UpdateEstimator, QEstimator, DoubleQEstimator, SARSAEstimator]
+__all__ = [UpdateEstimator, MaxQEstimator, DoubleQEstimator, SARSAEstimator]
