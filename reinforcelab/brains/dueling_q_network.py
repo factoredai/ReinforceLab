1cd073b4c5ae6d4597f9402dbb9df3fb1cea95d8
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-05-14 16:59:10 -0500

    Pass environment to brains

--- reinforcelab/brains/dueling_q_network.py
+++ reinforcelab/brains/dueling_q_network.py
@@ -1,33 +1,44 @@
 import torch
 import torch.nn as nn
-from itertools import zip_longest
 from .brain import Brain
+import gymnasium as gym
+from typing import Tuple
 
-from reinforcelab.utils import soft_update
+from reinforcelab.utils import soft_update, build_fcnn, space_is_type, get_state_action_sizes
 
 
 class DuelingQNetwork(nn.Module, Brain):
-    def __init__(self, state_size, action_size, hidden_layers=[], learning_rate=0.001, alpha=0.001):
+    def __init__(self, env: gym.Env, hidden_layers=[], learning_rate=0.001, alpha=0.001):
         super(DuelingQNetwork, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
+        self.state_size, self.action_size = self.__get_state_action_sizes(env)
         self.hidden_layers = hidden_layers
         self.learning_rate = learning_rate
         self.alpha = alpha
         self.model, self.value_head, self.advantage_head = self.__build_model()
 
+    def __get_state_action_sizes(self, env: gym.Env) -> Tuple[int, int]:
+        """Returns the state and action sizes established by the environment.
+        If the ennvironment isn't state continuous raises an error
+
+        Args:
+            env (gym.Env): Gymnasium environment
+
+        Returns:
+            Tuple[int, int]: State and action sizes
+        """
+        obs_cont = space_is_type(env.observation_space, gym.spaces.Box)
+        if not obs_cont:
+            raise RuntimeError("Incompatible observation space")
+
+        return get_state_action_sizes(env)
+
     def __build_model(self):
         layers_sizes = [self.state_size] + \
             self.hidden_layers + [self.action_size]
-        activations = [nn.ReLU()]*(len(layers_sizes) - 2)
-        layers_tuples = list(zip(layers_sizes, layers_sizes[1:]))
-        linear_layers = [nn.Linear(in_size, out_size)
-                         for in_size, out_size in layers_tuples[:-1]]
-        value_head = nn.Linear(layers_tuples[-1][0], 1)
-        advantage_head = nn.Linear(*layers_tuples[-1])
-        layers = [x for layer in zip_longest(
-            linear_layers, activations) for x in layer if x is not None]
-        return nn.Sequential(*layers), value_head, advantage_head
+        linear_layers = build_fcnn(layers_sizes[:-1])
+        value_head = build_fcnn([layers_sizes[-2], 1])
+        advantage_head = build_fcnn(layers_sizes[-2:])
+        return linear_layers, value_head, advantage_head
 
     def forward(self, state):
         hidden_activations = self.model(state)
--- reinforcelab/brains/q_network.py
+++ reinforcelab/brains/q_network.py
@@ -1,31 +1,41 @@
 import torch
 import torch.nn as nn
-from itertools import zip_longest
+import gymnasium as gym
+from typing import Tuple
 
 from .brain import Brain
-from reinforcelab.utils import soft_update
+from reinforcelab.utils import soft_update, build_fcnn, space_is_type, get_state_action_sizes
 
 
 class QNetwork(nn.Module, Brain):
-    def __init__(self, state_size, action_size, hidden_layers=[], learning_rate=0.01, alpha=0.001):
+    def __init__(self, env: gym.Env, hidden_layers=[], learning_rate=0.01, alpha=0.001):
         super(QNetwork, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
+        self.state_size, self.action_size = self.__get_state_action_sizes(env)
         self.hidden_layers = hidden_layers
         self.learning_rate = learning_rate
         self.alpha = alpha
         self.model = self.__build_model()
 
+    def __get_state_action_sizes(self, env: gym.Env) -> Tuple[int, int]:
+        """Returns the state and action sizes established by the environment.
+        If the ennvironment isn't state continuous raises an error
+
+        Args:
+            env (gym.Env): Gymnasium environment
+
+        Returns:
+            Tuple[int, int]: State and action sizes
+        """
+        obs_cont = space_is_type(env.observation_space, gym.spaces.Box)
+        if not obs_cont:
+            raise RuntimeError("Incompatible observation space")
+
+        return get_state_action_sizes(env)
+
     def __build_model(self):
         layers_sizes = [self.state_size] + \
             self.hidden_layers + [self.action_size]
-        activations = [nn.ReLU()]*(len(layers_sizes) - 2)
-        layers_tuples = list(zip(layers_sizes, layers_sizes[1:]))
-        linear_layers = [nn.Linear(in_size, out_size)
-                         for in_size, out_size in layers_tuples]
-        layers = [x for layer in zip_longest(
-            linear_layers, activations) for x in layer if x is not None]
-        return nn.Sequential(*layers)
+        return build_fcnn(layers_sizes)
 
     def forward(self, state):
         return self.model(state)
