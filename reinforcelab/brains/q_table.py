da6aeb68487415540840b92ffae33a8bdd882867
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-05-14 16:59:10 -0500

    Pass environment to brains

--- reinforcelab/brains/q_table.py
+++ reinforcelab/brains/q_table.py
@@ -2,16 +2,34 @@ import torch
 from torch import Tensor
 from typing import Tuple
 from collections import defaultdict
+import gymnasium as gym
 
 from .brain import Brain
+from reinforcelab.utils import space_is_type, get_state_action_sizes
 
 
 class QTable(Brain):
-    def __init__(self, state_size, action_size, alpha=0.01):
-        self.state_size = state_size
-        self.action_size = action_size
+    def __init__(self, env: gym.Env, alpha=0.01):
+        self.state_size, self.action_size = self.__get_state_action_sizes(env)
         self.alpha = alpha
-        self.table = defaultdict(lambda: torch.zeros(action_size))
+        self.table = defaultdict(lambda: torch.zeros(self.action_size))
+
+    def __get_state_action_sizes(self, env: gym.Env) -> Tuple[int, int]:
+        """Returns the state and action sizes established by the environment.
+        If the ennvironment isn't discrete in both state and action, raises an error
+
+        Args:
+            env (gym.Env): Gymnasium environment
+
+        Returns:
+            Tuple[int, int]: State and action sizes
+        """
+        obs_disc = space_is_type(env.observation_space, gym.spaces.Discrete)
+        action_disc = space_is_type(env.action_space, gym.spaces.Discrete)
+        if not obs_disc or not action_disc:
+            raise RuntimeError("Incompatible action/observation space")
+
+        return get_state_action_sizes(env)
 
     def __call__(self, state: Tensor) -> Tensor:
         # Assume that a batch of states were passed
