98a0462ed763bf98cb680ae7b9311571941f87ac
CarloCDT <carlo.dt@hotmail.com>
2023-06-28 18:35:45 -0500

    minor changes

--- leaderboard_bot/update_leaderboards.py
+++ leaderboard_bot/update_leaderboards.py
@@ -8,8 +8,9 @@ if len(sys.argv[1:-1]) > 0:
     for agent_path in sys.argv[1:-1]:
 
         environment_path = "/".join(agent_path.split("/")[:-1])
+        os.system(f'python -m pip install {environment_path}/requirements.txt')
         os.system(f'python {environment_path}/update_leaderboard.py {sys.argv[-1]}')
-else:
-    f = open(f"last_update.txt", "w")
-    f.write(f"Last Update (UTC): {datetime.utcnow()}\n\n")
-    f.close()
+
+f = open(f"leaderboard_bot/last_update.txt", "w")
+f.write(f"Last Update (UTC): {datetime.utcnow()}\n\n")
+f.close()
--- reinforcelab/tasks/mountaincar/optimized/update_leaderboard.py
+++ reinforcelab/tasks/mountaincar/optimized/update_leaderboard.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from tqdm import tqdm
+from reinforcelab.utils.leaderboard_utils import load_leaderboard, add_score, save_leaderboard, update_readme
 import dill
 import gymnasium as gym
 import numpy as np
@@ -7,42 +8,6 @@ import os
 import pandas as pd
 import sys
 import torch
-
-def load_leaderboard(path='/leaderboard/leaderboard.csv'):
-
-    path = "/".join((sys.argv[0]).split('/')[:-2]) + path
-
-    if os.path.exists(path):
-        return pd.read_csv(path)
-    else:
-        return pd.DataFrame(columns=['User', 'Score', "Num Epochs", 'Date (UTC)'])
-
-def add_score(leaderboard, user="test_user", score=0, num_epochs=0):
-    
-    new_score = pd.DataFrame.from_dict({'User': [user], 'Score': [score], "Num Epochs": [num_epochs], 'Date (UTC)': [datetime.utcnow()]})
-    leaderboard = pd.concat([leaderboard, new_score], axis=0)
-
-    # Sort leaderboard
-    leaderboard = leaderboard.sort_values(by=['Score', 'Num Epochs'], ascending=[0, 1]).reset_index(drop=True)
-
-    return leaderboard
-
-def save_leaderboard(leaderboard, path='/leaderboard/leaderboard.csv'):
-    path = "/".join((sys.argv[0]).split('/')[:-2]) + path
-    directory = "/".join(path.split('/')[:-1])
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-
-    leaderboard.to_csv(path, index=False)
-
-def update_readme(leaderboard):
-    path = "/".join((sys.argv[0]).split('/')[:-2])
-    f = open(f"{path}/README.md", "w")
-    f.write("# Leaderboard \n\n")
-    f.write(f"Last Update (UTC): {datetime.utcnow()}\n\n")
-    f.write(leaderboard.to_markdown())
-    f.write("\n")
-    f.close()
 
 def test(env, agent, num_episodes=100):
     rng = np.random.RandomState()
