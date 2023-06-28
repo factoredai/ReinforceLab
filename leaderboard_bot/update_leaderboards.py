5ffc036d02eb52c84b2eda4ac5c538fe760ed520
CarloCDT <carlo.dt@hotmail.com>
2023-06-28 18:51:50 -0500

    remove optimized agent

--- leaderboard_bot/update_leaderboards.py
+++ leaderboard_bot/update_leaderboards.py
@@ -8,7 +8,7 @@ if len(sys.argv[1:-1]) > 0:
     for agent_path in sys.argv[1:-1]:
 
         environment_path = "/".join(agent_path.split("/")[:-1])
-        os.system(f'python -m pip install {environment_path}/requirements.txt')
+        os.system(f'python -m pip install -r {environment_path}/requirements.txt')
         os.system(f'python {environment_path}/update_leaderboard.py {sys.argv[-1]}')
 
 f = open(f"leaderboard_bot/last_update.txt", "w")
