c71cfa1ccc824b4e3cb1f91efe360ca5a5576168
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-09-18 20:35:46 -0500

    Import all brains

--- reinforcelab/brains/__init__.py
+++ reinforcelab/brains/__init__.py
@@ -1,5 +1,6 @@
 from .brain import Brain
 from .q_table import QTable
 from .q_network import QNetwork
+from .actor_critic import ActorCritic
 
-__all__ = [Brain, QTable, QNetwork]
+__all__ = [Brain, QTable, QNetwork, ActorCritic]
